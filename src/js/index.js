0ebea46a6069e973188b27e1e659366ed5867c76
tungatarovM <tungatarov.96@mail.ru>
2018-11-03 08:36:26 +0500

    refactor Routes

--- src/js/index.js
+++ src/js/index.js
@@ -2,28 +2,49 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {
     Route,
-    Switch,
     BrowserRouter as Router,
 } from 'react-router-dom';
 
 import '../sass/main.scss';
 
+// COMPONENTS
 import Header from './components/Header';
 import  PrimaryForm from './components/Form';
+import Forecast from './components/Forecast';
 
 class App extends React.Component {
-    render() {
-      return (
-        <Router>
-          <div>
-            <Header />
-            <main>
-              <Route path="/" component={PrimaryForm} />
-            </main>
-          </div>
-        </Router>  
-      );
-    }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route render={(props) => {
+            return (<Header 
+              onClick={(city) => {
+                props.history.push({
+                  pathname: 'forecast',
+                  search: '?city=' + city,
+                });
+              }} 
+            />)
+          }} />
+
+          <main>
+            <Route path="/forecast"  component={Forecast} />
+            <Route exact path="/" render={function(props) {
+              return (<PrimaryForm  
+                onClick={(city) => {
+                  props.history.push({
+                    pathname: 'forecast',
+                    search: '?city=' + city
+                  })
+                }}
+              />)
+            }} />
+          </main>
+        </div>
+      </Router>  
+    );
+  }
 }
 
 ReactDom.render(
