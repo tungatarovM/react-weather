848e7046bdfb86a8da88dddeda5409c4ac915d65
tungatarovM <tungatarov.96@mail.ru>
2018-11-03 08:38:52 +0500

    add click handler function

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import HeaderForm from './HeaderForm';
 
-const Header = () => {
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
-        <header className="header">
-            <div className="header__title">
-                <h1 className="heading-primary">
-                    Main Header
-                </h1>
-            </div>
-            <HeaderForm />
-        </header>
-    );
-};
+      <header className="header">
+        <div className="header__title">
+          <h1 className="heading-primary">
+            Main Header
+          </h1>
+        </div>
+        <HeaderForm onClick={this.props.onClick} />
+    </header>
+    )
+  }
+}
+
 
 export default Header;
