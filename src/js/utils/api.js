633ca0f872fc85cf6d27671a2e8b36dd811eeb74
tungatarovM <tungatarov.96@mail.ru>
2018-11-09 06:35:05 +0500

    change the api function from promise to async await

--- src/js/utils/api.js
+++ src/js/utils/api.js
@@ -1,3 +1,5 @@
+import regeneratorRuntime from "regenerator-runtime";
+
 const appId = '26df31deaf1c433292f92242183110';
 const baseUrl = 'https://api.apixu.com/v1/';
 
@@ -22,26 +24,32 @@ const getQueryStringData = (city) => {
   };
 };
 
-export const getForecast = (city) => {
+
+export const getForecast = async (city) => {
   const queryStringData = getQueryStringData(city);
   const url = prepUrl('forecast.json', queryStringData);
 
-  return fetch(url)
-    .then(res => res.json())
-    .then(resObj => resObj.forecast.forecastday)
-    .then(days => days.map((day) => {
-      return {
-        date: day.date,
-        temp: {
-          maxTemp: day.day.maxtemp_c,
-          avgTemp: day.day.avgtemp_c,
-          minTemp: day.day.mintemp_c
-        },
-        condition: {
-          text: day.day.condition.text,
-          icon: `http://${day.day.condition.icon}`,
-        }
-      };
-    }))
+  const res = await fetch(url);
+  const body = await res.json();
+  const location = await body.location;
+  const forecastDays = await body.forecast.forecastday;
 
+  return await forecastDays.map((day) => {
+    return {
+      location: {
+        country: location.country,
+        region: location.name,
+      },
+      date: day.date,
+      temp: {
+        maxTemp: day.day.maxtemp_c,
+        avgTemp: day.day.avgtemp_c,
+        minTemp: day.day.mintemp_c
+      },
+      condition: {
+        text: day.day.condition.text,
+        icon: `http:${day.day.condition.icon}`,
+      }
+    };
+  });
 }
